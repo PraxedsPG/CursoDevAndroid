c5cb0eb82d79f73b29c7711f1552f7efef5706b1
Praxedes <112719512+PraxedsPG@users.noreply.github.com>
2023-06-28 10:53:35 -0300

    Customização

--- src/main/java/devandroid/vinicius/applistacurso/view/MainActivity.java
+++ src/main/java/devandroid/vinicius/applistacurso/view/MainActivity.java
@@ -1,10 +1,12 @@
-package devandroid.vinicius.applistacurso;
+package devandroid.vinicius.applistacurso.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import devandroid.vinicius.applistacurso.R;
+
+public class    MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
